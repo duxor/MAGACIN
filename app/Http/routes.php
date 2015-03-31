50537927ff0e24651f57ad284c9f2a3e478a6e9a
saxoman <jovicsasajovic@gmail.com>
2015-03-31 23:43:33 +0200

    search
    nije ok

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,3 +4,5 @@ Route::controller('/administracija/magacin','Magacin');
 Route::controller('/administracija/proizvod','Proizvod');
 Route::controller('/administracija','Administracija');
 Route::controller('/','Glavni');
+
+Route::post('/administracija/search', 'Search');
