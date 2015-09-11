3d2f3785f6f84205088f69628cf3154a09e7dc76
duxor <dullle@live.com>
2015-09-11 19:01:54 +0200

    А 1.3

--- resources/views/administracija/proizvodi.blade.php
+++ resources/views/administracija/proizvodi.blade.php
@@ -4,13 +4,86 @@
     @if(isset($proizvodi))
     @if($proizvodi)
         <h2 style="text-align: left" id="proizvodi"><i class="glyphicon glyphicon-qrcode"></i> Proizvodi
-            <button class="btn btn-primary" data-toggle="modal" data-target="#noviProizvod"><i class="glyphicon glyphicon-plus" data-toggle="tooltip" title="Dodaj novi proizvod"></i></button>
+            <button id="dugmeNovi" class="btn btn-primary" onClick="noviProizvod()"><i class="glyphicon glyphicon-plus" data-toggle="tooltip" title="Dodaj novi proizvod"></i></button>
             <div class="form-inline" style="float: right">
                 <button class="btn btn-sm btn-default" data-toggle="tooltip" title="Pronađi proizvod"><i class="glyphicon glyphicon-search"></i></button>
                 <div class="form-group">{!!Form::text('pretraga_proizvod',null,['class'=>'form-control'])!!}</div>
                 <div class="form-group">{!!Form::select('pretraga_vrsta_proizvoda',array_merge([0=>'Svi proizvodi'],$vrstaProizvoda),0,['class'=>'form-control'])!!}</div>
             </div>
         </h2>
+        <script>
+            function noviProizvod(proizvod){
+                //proizvod=null;
+                $('#dugmeNovi').fadeOut();
+                $('#work-place').hide();
+                $('#work-place').html('' +
+                '<hr><form action="/administracija/proizvod/proizvod" class="form-horizontal" id="forma" method="post" style="margin-top: 50px">'+
+                    (proizvod?'<input name="id" value="'+proizvod['id']+'" hidden="hidden">':'')+
+                    '<style>.fontResize *{font-size: 12px}</style>'+
+                    '<div class="col-sm-4 fontResize">' +
+                        '<img style="width: 100%;margin-bottom:20px" src="/img/default/slika-proizvoda.jpg">' +
+                        '<div class="form-group">' +
+                            '<div class="col-sm-12">' +
+                                '<select name="vrsta_proizvoda_id" class="form-control"><option value="0">Vrsta proizvoda</option></select>' +
+                            '</div>' +
+                        '</div>' +
+                        '<div id="dssifra" class="form-group has-feedback">' +
+                            '<div class="col-sm-12">' +
+                                '<input name="ssifra" value="'+(proizvod?proizvod['sifra']:'')+'" placeholder="Šifra" class="form-control" id="ssifra">' +
+                                '<span id="sssifra" class="glyphicon form-control-feedback"></span>' +
+                            '</div>' +
+                        '</div>' +
+                        '<div id="dnaziv" class="form-group has-feedback">' +
+                            '<div class="col-sm-12">' +
+                                '<input name="naziv" value="'+(proizvod?proizvod['naziv']:'')+'" placeholder="Naziv" class="form-control" id="naziv">' +
+                                '<span id="snaziv" class="glyphicon form-control-feedback"></span>' +
+                            '</div>' +
+                        '</div>' +
+                    '</div>' +
+                    '<div class="col-sm-7 fontResize">' +
+                        '<div class="form-group">' +
+                            '<div class="col-sm-offset-3 col-sm-9">' +
+                                '<input name="bar_kod" value="'+(proizvod?proizvod['bar_kod']:'')+'" placeholder="Bar kod" class="form-control">' +
+                            '</div>' +
+                        '</div>' +
+                        '<div class="form-group">' +
+                            '<div class="col-sm-offset-3 col-sm-9">' +
+                                '<input name="proizvodjac" value="'+(proizvod?proizvod['proizvodjac']:'')+'" placeholder="Proizvođač" class="form-control">' +
+                            '</div>' +
+                        '</div>' +
+                        '<div class="form-group">' +
+                            '<div class="col-sm-offset-3 col-sm-9">' +
+                                '<input name="jedinica_mjere" value="'+(proizvod?proizvod['jedinica_mjere']:'')+'" placeholder="Jedinica mjere" class="form-control">' +
+                            '</div>' +
+                        '</div>' +
+                        '<div class="form-group">' +
+                            '<div class="col-sm-offset-3 col-sm-9">' +
+                                '<input name="pakovanje_jedinica_mjere" value="'+(proizvod?proizvod['pakovanje_jedinica_mjere']:'')+'" placeholder="Pakovanje jedinica mjere" class="form-control">' +
+                            '</div>' +
+                        '</div>' +
+                        '<div class="form-group">' +
+                            '<div class="col-sm-offset-3 col-sm-9">' +
+                                '<input name="pakovanje_kolicina" value="'+(proizvod?proizvod['pakovanje_kolicina']:'')+'" placeholder="Pakovanje kolicina" class="form-control">' +
+                            '</div>' +
+                        '</div>' +
+                        '<div id="dopis" class="form-group has-feedback">' +
+                            '<div class="col-sm-offset-3 col-sm-9 ">' +
+                                '<textarea name="opis" placeholder="Opis" class="form-control" id="opis">'+(proizvod?proizvod['opis']:'')+'</textarea>' +
+                                '<span id="sopis" class="glyphicon form-control-feedback"></span>' +
+                            '</div>' +
+                        '</div>' +
+                        '<div class="form-group">' +
+                            '<div class="col-sm-3"></div>' +
+                            '<div class="col-sm-9">' +
+                                '<button class="btn btn-lg btn-primary" onClick="SubmitForm.submit(\'forma\')" type="button"><span class="glyphicon glyphicon-play-circle"></span> Sačuvaj</button>' +
+                                '<button class="btn btn-lg btn-warning" type="reset"><span class="glyphicon glyphicon-refresh"></span> Resetuj unos</button>' +
+                            '</div>' +
+                        '</div>' +
+                    '</div>' +
+                '</form>');
+                $('#work-place').fadeIn();
+            }
+        </script>
         <div id="work-place">
             <table class="table table-striped">
                 <thead>
